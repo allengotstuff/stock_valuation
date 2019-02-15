c47b3ecb1ebe6428f899a505296427499c5350a3
Allen Sun <allengotstuff@gmail.com>
2019-02-14 16:53:59 -0800

    error handle when can not find revenue

--- MarketCap.py
+++ MarketCap.py
@@ -7,15 +7,15 @@ import matplotlib.dates as mdates
 from Utility import RevUtility
 import plotly.graph_objs as go
 import plotly.plotly as py
-
+from FinDataApi import MarcoTrendsApi
 
 
 
 
 # api-endpoint
-company = "facebook"
+company = "NetEase"
 URL = "https://ycharts.com/charts/fund_data.json?securities=include%3Atrue%2Cid%3AFB%2C%2C&calcs=include%3Atrue%2Cid%3Amarket_cap%2C%2C&correlations=&format=real&recessions=false&zoom=5&startDate=&endDate=&chartView=&splitType=single&scaleType=linear&note=&title=&source=false&units=false&quoteLegend=true&partner=&quotes=&legendOnChart=true&securitylistSecurityId=&clientGroupLogoUrl=&displayTicker=false&ychartsLogo=&useEstimates=false&maxPoints=576"
-CSV_LOCATION = "/Users/allensun/Desktop/stock_valuation/fb_op_income.csv"
+CSV_LOCATION = "/Users/allensun/Desktop/stock_valuation/netease_operating_income.csv"
 
 class Network:
 
@@ -34,12 +34,14 @@ class Network:
 
 
 def main():
-    example = Network()
-    marke_cap = example.fetchData()['chart_data'][0][0]['raw_data']
+    # example = Network()
+    # marke_cap = example.fetchData()['chart_data'][0][0]['raw_data']
+
+    cap_info = MarcoTrendsApi().fetchMarketCapInfo("NTES")
 
     # read market cap data from api
-    x_val = [datetime.datetime.fromtimestamp(x[0]/1000) for x in marke_cap]
-    y_val = [x[1] for x in marke_cap]## in million usd
+    x_val = [x for x in cap_info[0]]
+    y_val = [x * 1000 for x in cap_info[1]]## in million usd
 
     utilityTool = RevUtility()
 
@@ -55,9 +57,11 @@ def main():
 
         revenue = utilityTool.findRevenueForGiveDate(current_date, revenueList)[1]
 
-        if revenue != None :
+        if revenue != None and revenue != 0:
             paybackRatio = cap/float(revenue)
             paybackTime.append(paybackRatio)
+        else:
+            paybackTime.append(0)
 
     data = [go.Scatter(x=x_val, y=paybackTime)]
     py.plot(data, filename = company)
